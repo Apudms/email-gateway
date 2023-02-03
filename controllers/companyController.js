048f3b5ddaafedee29e4a6dc2f567135a52b2227
Muhammad Saefudin <itsapud@gmail.com>
2023-02-03 08:29:18 +0700

    create CompanyController.js to controllers

---
+++ controllers/companyController.js
@@ -0,0 +1,21 @@
+const db = require('../models');
+const Company = db.company;
+const Contact = db.contact;
+
+class CompanyController {
+    static async getAll(req, res, next) {
+        const data = await Company.findAll();
+        if (data) {
+            res.status(200).json({
+                info: "find all success",
+                data: data
+            });
+        } else {
+            res.status(400).json({
+                info: "Company not found"
+            });
+        };
+    };
+};
+
+module.exports = CompanyController
