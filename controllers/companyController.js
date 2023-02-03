730e5fd55f11300ed124839ad655484ee36aee51
Muhammad Saefudin <itsapud@gmail.com>
2023-02-03 08:43:01 +0700

    Update: Get By ID Company include Contact

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -30,6 +30,42 @@ class CompanyController {
             });
         };
     };
+
+    static async getById(req, res, next) {
+        var id = parseInt(req.params.id);
+        const data = await Company.findByPk(id);
+        if (data) {
+            res.status(200).json({
+                info: "find with id success",
+                data: data
+            });
+        } else {
+            res.status(400).json({
+                info: "Company not found"
+            });
+        };
+    };
+
+    static async getByIdInclude(req, res, next) {
+        var id = parseInt(req.params.id);
+        const data = await Company.findOne({
+            where: { id: id },
+            include: [{
+                model: Contact,
+                as: 'contacts'
+            }]
+        });
+        if (data) {
+            res.status(200).json({
+                info: "find with id success",
+                data: data
+            });
+        } else {
+            res.status(400).json({
+                info: "Company not found"
+            });
+        };
+    };
 };
 
 module.exports = CompanyController
